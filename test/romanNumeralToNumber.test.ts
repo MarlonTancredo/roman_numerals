01cbbf469b3ed137ceb7a444c9ec3e4c7f476d73
Marlon Tancredo <marlon_tancredo@hotmail.com>
2023-08-10 18:53:18 +0100

    refactor: refactoring romanNumeralToNumber.test.ts

--- test/romanNumeralToNumber.test.ts
+++ test/romanNumeralToNumber.test.ts
@@ -1,9 +1,27 @@
-import { getRomanNumeralToUpperCase } from "../src/romanTonumber";
+import {
+  getRomanNumeralToUpperCase,
+  printDecimalNumber,
+} from "../src/romanNumeralToNumber";
 
 describe("getRomanNumeralToUpperCase()", () => {
-  test("should return any string to upperCase ", () => {
+  it("should return any string to upperCase ", () => {
     //Arrange
     const romanNumeral = "i";
+
+    //Act Assert
     expect(getRomanNumeralToUpperCase(romanNumeral)).toBe("I");
+  });
+});
+
+describe("printDecimalNumber()", () => {
+  it("should return 4 ", () => {
+    //Arrange
+    const romanNumeral = "xxiv";
+
+    //Act
+    getRomanNumeralToUpperCase(romanNumeral);
+
+    //Assert
+    expect(printDecimalNumber()).toBe(24);
   });
 });
