324d4378725f3d054c3e561052630ee353c09535
Marlon Tancredo <marlon_tancredo@hotmail.com>
2023-08-11 20:54:27 +0100

    refactor: refacoring index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,19 @@
-import { convertToNumber } from "./src/romanNumeralToNumber";
+import {
+  convertToNumber,
+  isLetterUpperCase,
+  isValidNumber,
+} from "./src/romanNumeralToNumber";
 import { convertToRoman } from "./src/numberToRomanNumeral";
 
-convertToNumber("IV");
+const printNumber = (letter: string) => {
+  if (!isLetterUpperCase(letter)) {
+    return "You must to enter a uppercase letter!";
+  }
+  if (isValidNumber(convertToNumber(letter))) {
+    return "You must enter a valid Roman number!";
+  }
+  return `Convert to Number: ${convertToNumber(letter)}`;
+};
+console.log(printNumber("XXXIV"));
 
 console.log("Convert to Roman numeral:", convertToRoman(1001));
