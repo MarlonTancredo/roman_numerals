5bb9ef8aa81b38da362ee87a4100340b71e2b4d8
Marlon Tancredo <marlon_tancredo@hotmail.com>
2023-08-11 20:56:45 +0100

    refactor: refacoring romanNumeralToNumber.test.ts

--- test/romanNumeralToNumber.test.ts
+++ test/romanNumeralToNumber.test.ts
@@ -1,11 +1,26 @@
-import { convertToNumber } from "../src/romanNumeralToNumber";
+import {
+  convertToNumber,
+  isValidNumber,
+  isLetterUpperCase,
+} from "../src/romanNumeralToNumber";
 
 describe("convertToNumber()", () => {
-  it("should return 24 ", () => {
+  it("should return 24", () => {
     //Arrange
     const romanNumeral = "XXIV";
 
     //Act Assert
     expect(convertToNumber(romanNumeral)).toBe(24);
+  });
+  it("should return true", () => {
+    //Arrange Act Assert
+    expect(isValidNumber(NaN)).toBe(true);
+  });
+  it("should return false", () => {
+    //Arrange
+    const romanNumeral = "i";
+
+    //Act Assert
+    expect(isLetterUpperCase(romanNumeral)).toBe(false);
   });
 });
