58345bb5177325723231ddf9ff5b946fc8e9cc09
Marlon Tancredo <marlon_tancredo@hotmail.com>
2023-08-11 20:55:22 +0100

    refactor: refacoring numberToRomanNumeral.test.ts

--- test/numberToRomanNumeral.test.ts
+++ test/numberToRomanNumeral.test.ts
@@ -2,19 +2,28 @@ import { convertToRoman } from "../src/numberToRomanNumeral";
 
 describe("convertToRoman()", () => {
   it("should return M", () => {
-    //Arrange Act Assert
-    expect(convertToRoman(1000)).toBe("M");
+    //Arrange
+    const number = 1000;
+
+    //Act Assert
+    expect(convertToRoman(number)).toBe("M");
   });
   it("should return a message", () => {
-    //Arrange Act Assert
-    expect(convertToRoman(4000)).toBe(
-      "\nThe range of Roman numeral are 3999, try a number lower then 4000."
-    );
+    //Arrange
+    const number = 4000;
+    const message =
+      "\nThe range of Roman numeral are 3999, try a number lower then 4000.";
+
+    //Act Assert
+    expect(convertToRoman(number)).toBe(message);
   });
   it("should return a message", () => {
-    //Arrange Act Assert
-    expect(convertToRoman(0)).toBe(
-      "\nThere's no zero number in Roman numeral, try a number bigger then ZERO."
-    );
+    //Arrange
+    const number = 0;
+    const message =
+      "\nThere's no zero number in Roman numeral, try a number bigger then ZERO.";
+
+    //Act Assert
+    expect(convertToRoman(number)).toBe(message);
   });
 });
